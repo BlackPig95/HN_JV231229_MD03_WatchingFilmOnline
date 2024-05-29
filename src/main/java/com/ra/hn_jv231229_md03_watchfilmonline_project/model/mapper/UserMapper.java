142a583061c5346d64452204c93400870b9f2e74
binh-1892001 <=>
2024-05-29 19:51:00 +0700

    update commit

---
+++ src/main/java/com/ra/hn_jv231229_md03_watchfilmonline_project/model/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.ra.hn_jv231229_md03_watchfilmonline_project.model.mapper;
+
+import com.ra.hn_jv231229_md03_watchfilmonline_project.model.dto.UserDTO;
+import com.ra.hn_jv231229_md03_watchfilmonline_project.model.entity.User;
+
+public class UserMapper {
+    public static UserDTO toUserDTO(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getUserRole().name(),
+                user.getCreatedAt(),
+                user.getStatus()
+        );
+    }
+}
+
