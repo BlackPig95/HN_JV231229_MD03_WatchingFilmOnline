2b994edb879b414f28423df841f73c84e689297c
BlackPig95 <thisisblackpig@gmail.com>
2024-05-30 19:00:11 +0700

    hieu-optimization

--- src/main/java/com/ra/hn_jv231229_md03_watchfilmonline_project/controller/HomePageController.java
+++ src/main/java/com/ra/hn_jv231229_md03_watchfilmonline_project/controller/HomePageController.java
@@ -36,16 +36,18 @@ public class HomePageController
     private final IFilmEpisodeService episodeService;
     private final IUserService userService;
     private final ICommentService commentService;
-    @Autowired
-    private IBannerService bannerService;
+    private final IBannerService bannerService;
+    private final HttpSession session;
 
     @Autowired
-    public HomePageController(IFilmService filmService, IFilmEpisodeService episodeService, IUserService userService, ICommentService commentService)
+    public HomePageController(IFilmService filmService, IFilmEpisodeService episodeService, IUserService userService, ICommentService commentService, IBannerService bannerService, HttpSession session)
     {
         this.filmService = filmService;
         this.episodeService = episodeService;
         this.userService = userService;
         this.commentService = commentService;
+        this.bannerService = bannerService;
+        this.session = session;
     }
 
     @RequestMapping("/")
@@ -79,6 +81,12 @@ public class HomePageController
         model.addAttribute("seriesFilm", responseSeriesList);
         model.addAttribute("singleFilm", responseSingleList);
         model.addAttribute("filmList", responseFilmList);
+        
+        User user = (User) session.getAttribute("user");
+        if (user != null)
+        {
+            model.addAttribute("username", user.getUsername());
+        }
         return "index";
     }
 
